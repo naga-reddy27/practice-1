6931061bbb8613bb3ba6840c7948c9525f5010ee
naga-reddy27 <mns.reddy07@gmail.com>
2024-09-03 18:36:22 +0100

    files added
    git commit -m "commited"

---
+++ var.tf
@@ -0,0 +1,14 @@
+variable "AMI" {
+    description = "This variable having the AMI value"
+    default = "ami-0aa7d40eeae50c9a9"
+}
+
+variable "InstanceType" {
+    description = "This variable having the Instance Type value"
+    default = "t2.micro"
+}
+
+variable "NumberofInstances" {
+    description = "This variable having the Number of Instances value"
+    default = "3"
+}
